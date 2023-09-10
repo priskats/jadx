cdf47020891cd4a51c85a8112ca60d93ba4664c2
priskats <144108340+priskats@users.noreply.github.com>
2023-09-10 18:51:53 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,14 @@ plugins {
 	id("com.github.ben-manes.versions") version "0.48.0"
 	id("se.patrikerdes.use-latest-versions") version "0.2.18"
 	id("com.diffplug.spotless") version "6.21.0"
+	id("org.sonarqube") version "4.2.1.3168"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "Newproject")
+    property("sonar.projectName", "Newproject")
+  }
 }
 
 val jadxVersion by extra { System.getenv("JADX_VERSION") ?: "dev" }
